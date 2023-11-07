d8b04d0500344402a0f2558f0b0c59fe25f52e29
kmb8043 <you@example.com>
2023-11-06 22:39:20 -0500

    updates

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# Password-Generator
+# Password Generator #
+
+## Overview ##
+
+This is website that helps generate a random password based off of criteria the user answers.
+
+
+## Description ##
+
+I added in the JavaScript files to this existing html and css. 
+The user will click the "Generate Password" button. 
+After clicking, prompts about length, uppercase, lowercase, numbers and special characters will appear. 
+The user will determind the length of the password and if they want any of the other characters included into their password. 
+After the user completes all the prompts, a password will generate. 
+The user can then copty the password to use. 
+
+## Usage ##
+
+This website is meant to help with creating a secure password. 
+
+![password](https://github.com/kmb8043/Password-Generator/assets/147110705/8bf14bb7-ae03-4011-8887-483fabe78d9e)
+
+
+## Link to site ##
+
+
+
+## Licence ##
+MIT
--- script.js
+++ script.js
@@ -1,14 +1,75 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// variables
+var upperCase = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+var lowerCase = ["abcdefghijklmnopqrstuvwxyz"];
+var numbers = ["0123456789"];
+var specialCharacters = ["!@$#%&*?"];
+var randomPassword = "";
+
+
+
+function generatePassword(){
+// prompt length
+var length = window.prompt("How long do you want your password be?");
+
+  if (length < 8 || length > 128 || isNaN(length)){
+    alert("Invalid - Password length must be between 8-128 characters long");
+    return;
+  }
+
+// prompt uppercase
+var BIG = window.confirm ("Click OK to include UPPERCASE");
+  function upperCase(){
+    return String.fromCharCode(Math.floor(Math.random() * 26 ) +65);
+  }
+console.log(upperCase());
+
+
+// prompt lowercase
+var small = window.confirm ("Click OK to include lowercase");
+  function lowerCase(){
+    return String.fromCharCode(Math.floor(Math.random() * 26 ) + 97);
+}
+console.log(lowerCase());
+
+  
+// promt numers
+var num = window.confirm ("Click OK to include numbers.");
+function numbers(){
+  return String.fromCharCode(Math.floor(Math.random() * 10 ) + 48);
+}
+
+console.log(numbers());
+
+
+// prompt special characters
+var symbols = window.confirm ("Click OK to include special characters.");
+  function specialCharacters(){
+    return specialCharacters[Math.floor(Math.random() * specialCharacters.length)];
+    }
+  
+  console.log(specialCharacters());
+
+
+//verify at least 1 criteria is met
+if(BIG === false && small === false && num === false && symbols === false){
+  window.alert("You must choose at least 1 type of character to include.");
+}
+
+}
+
+//display password
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+//event listener to create button
+generateBtn.addEventListener("click", writePassword)
