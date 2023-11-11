ac32cd55b77d456ea761d42de9351b97a7f67b9e
kmb8043 <you@example.com>
2023-11-10 20:06:00 -0500

    edit

--- script.js
+++ script.js
@@ -1,64 +1,81 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// variables
-var upperCase = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
-var lowerCase = ["abcdefghijklmnopqrstuvwxyz"];
-var numbers = ["0123456789"];
-var specialCharacters = ["!@$#%&*?"];
-var randomPassword = "";
-
+// password options
+ var passwordOptions = {
+  upperCase :"ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+  lowerCase : "abcdefghijklmnopqrstuvwxyz",
+  numbers : "0123456789",
+  specialCharacters : "!@$#%&*?"
+}
 
 
 function generatePassword(){
+
+  var passOpt = "";
+
+  var passConfirm = "";
+
 // prompt length
 var length = window.prompt("How long do you want your password be?");
 
-  if (length < 8 || length > 128 || isNaN(length)){
+//if user types a length invalid
+  if (length < 8 || length > 128){
     alert("Invalid - Password length must be between 8-128 characters long");
     return;
   }
 
 // prompt uppercase
-var BIG = window.confirm ("Click OK to include UPPERCASE");
-  function upperCase(){
-    return String.fromCharCode(Math.floor(Math.random() * 26 ) +65);
-  }
-console.log(upperCase());
+var yesUpper = window.confirm ("Click OK to include UPPERCASE characters");
+  
+// if user confirms uppercase
+if(yesUpper){
+  passOpt += passwordOptions.upperCase;
+  console.log(yesUpper);
+}
 
 
 // prompt lowercase
-var small = window.confirm ("Click OK to include lowercase");
-  function lowerCase(){
-    return String.fromCharCode(Math.floor(Math.random() * 26 ) + 97);
+var yesLower = window.confirm ("Click OK to include lowercase characters");
+
+//if user confirms lowercase
+if(yesLower){
+  passOpt += passwordOptions.lowerCase;
+  console.log(yesLower);
+
 }
-console.log(lowerCase());
 
   
 // promt numers
-var num = window.confirm ("Click OK to include numbers.");
-function numbers(){
-  return String.fromCharCode(Math.floor(Math.random() * 10 ) + 48);
-}
+var yesNums = window.confirm ("Click OK to include numbers.");
 
-console.log(numbers());
+//if user confirms numbers
+if(yesNums){
+  passOpt += passwordOptions.numbers;
+  console.log(yesNums);
+}
 
 
 // prompt special characters
-var symbols = window.confirm ("Click OK to include special characters.");
-  function specialCharacters(){
-    return specialCharacters[Math.floor(Math.random() * specialCharacters.length)];
-    }
-  
-  console.log(specialCharacters());
+var yesSymbols = window.confirm ("Click OK to include special characters.");
+
+//if user confirms special characters
+if(yesSymbols){
+  passOpt += passwordOptions.specialCharacters;
+  console.log(yesSymbols);
+}
 
 
 //verify at least 1 criteria is met
-if(BIG === false && small === false && num === false && symbols === false){
+if(yesUpper === false && yesLower === false && yesNums === false && yesSymbols === false){
   window.alert("You must choose at least 1 type of character to include.");
-}
+  return(generatePassword);
+};
 
+function passConfirm (fromString){
+  return fromString[Math.floor(Math.random() * fromString.length)];
 }
+};
 
 //display password
 
